972219ed96a6e5b364c44c3b460a0f373f39320e
Tony Aldridge <zaragopha@hotmail.com>
2014-01-19 20:38:12 +0000

    Fiddling

--- src/tweetgrabber.erl
+++ src/tweetgrabber.erl
@@ -16,6 +16,8 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
+-compile([export_all]).
+
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
@@ -54,10 +56,10 @@ code_change(_OldVsn, State, _Extra) ->
 build_oauth_details(Consumer_key, Access_token) ->
     [
         {oauth_consumer_key, Consumer_key},
-        {oauth_nonce, erlang:timestamp()}, %FIXME: Should be unix time
+        {oauth_nonce, unix_time()},
         {oauth_signature_method, "HMAC-SHA1"},
         {oath_token, Access_token},
-        {oath_timestamp, erlang:timestamp()},
+        {oath_timestamp, unix_time()},
         {oath_version, "1.0"}
     ].
 
@@ -76,5 +78,9 @@ add_oauth_signature(Details, Oauth_signature) ->
 
 build_authorization_header(Oauth_details) ->
     "Authorization: OAuth " ++ string:join([lists:flatten(io_lib:format("~p=\"~p\"", [Key, Value])) || {Key, Value} <- Oauth_details], ", ").
+
+unix_time() ->
+    {Megasecs, Secs, _Microsecs} = erlang:timestamp(),
+    Mega * 1000000 + Secs.
 
 % http://stackoverflow.com/questions/12916539/simplest-php-example-for-retrieving-user-timeline-with-twitter-api-version-1-1
