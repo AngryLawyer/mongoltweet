98fb1f98ef7278770bb698672b2c2fb648026242
Tony Aldridge <zaragopha@hotmail.com>
2014-01-25 12:44:14 +0000

    Can now fetch tweets from a specific user

--- src/tweetgrabber.erl
+++ src/tweetgrabber.erl
@@ -10,7 +10,7 @@
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/1]).
+-export([start_link/1, fetch/1]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -19,8 +19,6 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--compile([export_all]).
-
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
@@ -28,8 +26,8 @@
 start_link(Params) ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, Params, []).
 
-test_fetch() ->
-    gen_server:call(?MODULE, test).
+fetch(Screen_name) ->
+    gen_server:call(?MODULE, {fetch, Screen_name}).
 
 
 %% ------------------------------------------------------------------
@@ -39,12 +37,12 @@ test_fetch() ->
 init(Args) ->
     {ok, Args}.
 
-handle_call(test, _From, State) ->
+handle_call({fetch, Screen_name}, _From, State) ->
     Consumer_key = proplists:get_value(consumer_key, State),
     Access_token = proplists:get_value(access_token, State),
     Consumer_secret = proplists:get_value(consumer_secret, State),
     Access_token_secret = proplists:get_value(access_token_secret, State),
-    {reply, request(Consumer_key, Access_token, Consumer_secret, Access_token_secret), State};
+    {reply, request(Consumer_key, Access_token, Consumer_secret, Access_token_secret, Screen_name), State};
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
@@ -64,10 +62,10 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
-request(Consumer_key, Access_token, Consumer_secret, Access_token_secret) ->
+request(Consumer_key, Access_token, Consumer_secret, Access_token_secret, Screen_name) ->
     Url = "https://api.twitter.com/1.1/statuses/user_timeline.json",
     Consumer = {Consumer_key, Consumer_secret, hmac_sha1},
-    {ok, Response} = oauth:get(Url, [], Consumer, Access_token, Access_token_secret),
+    {ok, Response} = oauth:get(Url, [{screen_name, Screen_name}], Consumer, Access_token, Access_token_secret),
     Response.
 
 -ifdef(TEST).
