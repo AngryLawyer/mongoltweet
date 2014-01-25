f32668c9b77223025d075dd3113bd8ef59b90b0b
Tony Aldridge <zaragopha@hotmail.com>
2014-01-25 12:35:44 +0000

    Now can actually get stuff out of twitter

--- src/mongoltweet_app.erl
+++ src/mongoltweet_app.erl
@@ -13,11 +13,7 @@ start() ->
     start([], []).
 
 start(_StartType, _StartArgs) ->
-    ssl:start(),
-    lhttpc:start(),
     mongoltweet_sup:start_link().
 
 stop(_State) ->
-    lhttpc:stop(),
-    ssl:stop(),
     ok.
