6e7d5053ca6fff2e75fe2a5d6e9d5b906aed80f1
Tony Aldridge <zaragopha@hotmail.com>
2014-02-01 12:17:38 +0000

    Attempting to return unicode from webmachine

--- src/translate_resource.erl
+++ src/translate_resource.erl
@@ -10,4 +10,11 @@
 init([]) -> {ok, undefined}.
 
 to_html(ReqData, State) ->
-    {"<html><body>Hello, new world</body></html>", ReqData, State}.
+    {Author, _Time, Tweet} = unpack_tweet(worker:get_unsolved_tweet()),
+    %{io_lib:format("<html><body><p>~ts</p><p>~ts</p></body></html>", [Author, <<Tweet/utf8>>]), ReqData, State}.
+    {encode_uri_rfc3986:encode(Tweet), ReqData, State}.
+
+unpack_tweet({tweet, _, Author, Time, Tweet, _, _}) ->
+    {Author, Time, Tweet};
+unpack_tweet(undefined) ->
+    {"NONE", 0, "NO TWEETS FOUND"}.
