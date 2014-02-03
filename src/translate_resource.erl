1fad3572f92f3d7b0b2f58033ef90bcec5ff9c37
Tony Aldridge <tony@dabapps.com>
2014-02-03 14:13:47 +0000

    Almost got UTF8 working

--- src/translate_resource.erl
+++ src/translate_resource.erl
@@ -3,7 +3,7 @@
 %% @doc Example webmachine_resource.
 
 -module(translate_resource).
--export([init/1, to_html/2]).
+-export([init/1, to_html/2, charsets_provided/2, convert/1]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
@@ -11,10 +11,17 @@ init([]) -> {ok, undefined}.
 
 to_html(ReqData, State) ->
     {Author, _Time, Tweet} = unpack_tweet(worker:get_unsolved_tweet()),
-    %{io_lib:format("<html><body><p>~ts</p><p>~ts</p></body></html>", [Author, <<Tweet/utf8>>]), ReqData, State}.
-    {encode_uri_rfc3986:encode(Tweet), ReqData, State}.
+    Encoded_tweet = unicode:characters_to_binary(Tweet),
+    Formatted = io_lib:format("<html><body><p>~ts</p><p>~ts</p></body></html>", [Author, Encoded_tweet]), %FIXME: Needs to produce a binary
+    {Encoded_tweet, ReqData, State}.
+
+charsets_provided(ReqData, State) ->
+    {[{"utf8", fun convert/1}], ReqData, State}.
 
 unpack_tweet({tweet, _, Author, Time, Tweet, _, _}) ->
     {Author, Time, Tweet};
 unpack_tweet(undefined) ->
     {"NONE", 0, "NO TWEETS FOUND"}.
+
+convert(Body) ->
+    Body.
