6e77d6db9b6b0890812b2759192c4cee5bcacb3f
Tony Aldridge <tony@dabapps.com>
2014-02-17 13:54:43 +0000

    Switched to using DTL

---
+++ src/history_resource.erl
@@ -0,0 +1,21 @@
+%% @author author <author@example.com>
+%% @copyright YYYY author.
+%% @doc Example webmachine_resource.
+
+-module(history_resource).
+-export([init/1, to_html/2, charsets_provided/2]).
+
+-include_lib("webmachine/include/webmachine.hrl").
+
+init(Params) -> {ok, Params}.
+
+
+to_html(ReqData, State) ->
+    {ok, Content} = history_dtl:render([]),
+    {Content, ReqData, State}.
+
+charsets_provided(ReqData, State) ->
+    {[{"utf8", fun convert/1}], ReqData, State}.
+
+convert(Body) ->
+    Body.
